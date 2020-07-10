45b6072a463e27b6c22624132872aa2b7458451f
Camilo <camilorivera86@gmail.com>
2020-07-10 17:02:45 -0300

    Separated ongoing, incoming and past launches

--- src/components/launchCard.js
+++ src/components/launchCard.js
@@ -6,15 +6,17 @@ import CardContent from '@material-ui/core/CardContent';
 import LinearProgress from '@material-ui/core/LinearProgress'
 
 const LaunchCard = ({ node }) => {
-  const daysLeft = useMemo(() => {
-    return dayjs(node.date).diff(dayjs(), 'day')
-  }, [node])
-  console.log(daysLeft)
+  const daysDiff = useMemo(() => (
+    dayjs(node.date).diff(dayjs(), 'day')
+  ), [node])
+
   return (
     <Card>
       <CardContent>
         <Typography>
-          {`${daysLeft} days left until ${node.title}.`}
+          { daysDiff === 0 && `${node.title} is live!`}
+          { daysDiff > 0 && `${daysDiff} days left until ${node.title}.`}
+          { daysDiff < 0 && `${node.title} (${daysDiff * -1} days ago).`}
         </Typography>
       </CardContent>
     </Card>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
+import dayjs from 'dayjs'
 import { graphql } from "gatsby"
-import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 
@@ -8,23 +10,54 @@ import SEO from "../components/seo"
 import LaunchCard from "../components/launchCard"
 
 const IndexPage = ({ data }) => {
+  const ongoingLaunches = data.allStrapiLaunch.edges.filter(({ node }) => {
+    const hoursDiff = dayjs(node.date).diff(dayjs(), 'hour')
+    return hoursDiff > -24 && hoursDiff < 0
+  })
+
   const incomingLaunches = data.allStrapiLaunch.edges.filter(({ node }) => (
     Date.now() - new Date(node.date) < 0
-  )).sort((nodeA, nodeB) => nodeA.date - nodeB.date)
+  )).sort((a, b) => new Date(a.node.date) - new Date(b.node.date))
+
   const pastLaunches = data.allStrapiLaunch.edges.filter(({ node }) => (
-    Date.now() - new Date(node.date) > 0
-  ))
-  console.log('incomingLaunches', incomingLaunches)
-  console.log('pastLaunches', pastLaunches)
-  incomingLaunches.forEach(({ node }) => console.log(node.id, node.date))
-  pastLaunches.forEach(({ node }) => console.log(node.id, node.date))
+    !ongoingLaunches.find(({ node: ongoingLaunch }) => (
+      ongoingLaunch.id === node.id
+    )) && Date.now() - new Date(node.date) > 0
+  )).sort((a, b) => new Date(b.node.date) - new Date(a.node.date))
+
   return (
     <>
       <SEO title="Home" />
       <Container>
         <Box mt={2}>
+          <Typography variant="h1">
+            SpaceX launches
+          </Typography>
+          <Typography variant="h2">
+            Ongoing launches
+          </Typography>
+          <Grid container spacing={2}>
+            {ongoingLaunches.map(({ node }) => (
+              <Grid key={node.id} item xs={6} sm={4} md={2}>
+                <LaunchCard node={node} />
+              </Grid>
+            ))}
+          </Grid>
+          <Typography variant="h2">
+            Incoming launches
+          </Typography>
           <Grid container spacing={2}>
             {incomingLaunches.map(({ node }) => (
+              <Grid key={node.id} item xs={6} sm={4} md={2}>
+                <LaunchCard node={node} />
+              </Grid>
+            ))}
+          </Grid>
+          <Typography variant="h2">
+            Past launches
+          </Typography>
+          <Grid container spacing={2}>
+            {pastLaunches.map(({ node }) => (
               <Grid key={node.id} item xs={6} sm={4} md={2}>
                 <LaunchCard node={node} />
               </Grid>
